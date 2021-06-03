6bb4695d99c9b9dc51d59e471a0a0775b99b90b8
Quinn Cheong <quinncheong@hotmail.com>
2021-06-03 19:22:42 +0800

    more changes

---
+++ unpack/DecompileMe.exe/DecompileMe.py.py
@@ -0,0 +1,31 @@
+# uncompyle6 version 2.11.5
+# Python bytecode 2.7 (62211)
+# Decompiled from: Python 2.7.18 (v2.7.18:8d21aa21f2, Apr 20 2020, 13:25:05) [MSC v.1500 64 bit (AMD64)]
+# Embedded file name: DecompileMe.py
+# Compiled at: 2020-07-04 12:27:36
+import ctypes
+FLAG = 'CDDC20'
+FLAG += '{'
+FLAG += 'N'
+FLAG += 'i'
+FLAG += 'C'C c
+FLAG += 'e'
+FLAG += '-'
+FLAG += '2'
+FLAG += '-'
+FLAG += 'M'
+FLAG += 'e'
+FLAG += 'e'
+FLAG += 'T'
+FLAG += '-'
+FLAG += 'p'
+FLAG += 'y'
+FLAG += '2'
+FLAG += 'e'
+FLAG += 'x'
+FLAG += 'e'
+FLAG += '~'
+FLAG += ':'
+FLAG += 'D'
+FLAG += '}'
+ctypes.windll.user32.MessageBoxA(0, 'Hello py2exe!', 'DecompileMe', 0)
